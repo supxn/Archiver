ccd7f24bad34f842161842d50e7655d4544a0d11
supxn <116300094+supxn@users.noreply.github.com>
2024-05-17 00:24:30 +0400

    Добавьте файлы проекта.

---
+++ Archiver/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using Archiver;
+
+static Dictionary<char, int> CalculateFrequencies(string text)
+{
+    var frequencies = new Dictionary<char, int>();
+    foreach (var ch in text)
+    {
+        if (frequencies.ContainsKey(ch))
+            frequencies[ch]++;
+        else
+            frequencies[ch] = 1;
+    }
+    return frequencies;
+}
+string text = "some mistakes with file stream";
+//using (StreamReader file = new StreamReader("C:/Users/uskov/source/repos/Archiver/TextFile1.txt"))
+//{
+//    text = file.ReadToEnd();
+
+//}
+//Console.WriteLine("Текст из файла:", text);
+
+var frequencies = CalculateFrequencies(text);
+var huffmanTree = new haffman(frequencies);
+var codes = huffmanTree.BuildCodes();
+
+Console.WriteLine("Character\tFrequency\tCode");
+int Value = 0;
+foreach (var kvp in frequencies)
+{
+    Value++;
+    Console.WriteLine($"{kvp.Key}\t\t{kvp.Value}\t\t{codes[kvp.Key]}");
+}
+
+
+using (StreamWriter file = new StreamWriter("C:/Users/uskov/source/repos/Archiver/Archiving.txt"))
+{
+    //get meta
+    file.Write(Value);
+    foreach (var kvp in frequencies)
+    {
+        string CodeString = codes[kvp.Key];
+        int ValueOfBits=CodeString.Length;
+        file.Write($"{kvp.Key}{ValueOfBits}{codes[kvp.Key]}");
+    }
+    //get data
+    foreach(char symbol in text)
+    {
+   
+        file.Write(codes[symbol]);
+    }
+    file.Close();
+}
---
+++ Archiver/haffman.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using static Archiver.haffman;
+
+namespace Archiver
+{
+    internal class haffman
+    {
+        public class HuffmanNode
+        {
+            public char Symbol { get; set; }
+            public int Frequency { get; set; }
+            public int h { get; set; }
+            public HuffmanNode Left { get; set; }
+            public HuffmanNode Right { get; set; }
+        }
+
+
+        private Dictionary<char, int> _frequencies;
+
+        public haffman(Dictionary<char, int> frequencies)
+        {
+            _frequencies = frequencies;
+        }
+
+        public HuffmanNode BuildTree()
+        {
+            var priorityQueue = new SortedDictionary<int, List<HuffmanNode>>();
+
+            // Create a priority queue sorted by frequency
+            foreach (var kvp in _frequencies)
+            {
+                var node = new HuffmanNode { Symbol = kvp.Key, Frequency = kvp.Value };
+                if (!priorityQueue.ContainsKey(node.Frequency))
+                    priorityQueue[node.Frequency] = new List<HuffmanNode>();
+                priorityQueue[node.Frequency].Add(node);
+            }
+
+            // Construct Huffman Tree
+            while (priorityQueue.Count > 1)
+            {
+                var firstMinFreq = priorityQueue.First();
+                var firstMinNode = firstMinFreq.Value.First();
+                firstMinFreq.Value.RemoveAt(0);
+                if (firstMinFreq.Value.Count == 0)
+                    priorityQueue.Remove(firstMinFreq.Key);
+
+                var secondMinFreq = priorityQueue.First();
+                var secondMinNode = secondMinFreq.Value.First();
+                secondMinFreq.Value.RemoveAt(0);
+                if (secondMinFreq.Value.Count == 0)
+                    priorityQueue.Remove(secondMinFreq.Key);
+
+                var newNode = new HuffmanNode
+                {
+                    Frequency = firstMinNode.Frequency + secondMinNode.Frequency,
+                    Left = firstMinNode,
+                    Right = secondMinNode
+                };
+
+                if (!priorityQueue.ContainsKey(newNode.Frequency))
+                    priorityQueue[newNode.Frequency] = new List<HuffmanNode>();
+                priorityQueue[newNode.Frequency].Add(newNode);
+            }
+
+            return priorityQueue.First().Value.First();
+        }
+
+        public Dictionary<char, string> BuildCodes()
+        {
+            var root = BuildTree();
+            var codes = new Dictionary<char, string>();
+            TraverseTree(root, "", codes);
+            return codes;
+        }
+
+        private void TraverseTree(HuffmanNode node, string code, Dictionary<char, string> codes)
+        {
+            if (node == null)
+                return;
+            if (node.Left == null && node.Right == null)
+                codes[node.Symbol] = code;
+            TraverseTree(node.Left, code + "0", codes);
+            TraverseTree(node.Right, code + "1", codes);
+        }
+
+    }
+  
+}
+
+
+       
+            
+
+           
+        
+
